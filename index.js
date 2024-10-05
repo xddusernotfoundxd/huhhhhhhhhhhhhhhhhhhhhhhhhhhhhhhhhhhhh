efc84ec1164ce2079ab9dcac44bbb26b6006b30d
xddusernotfoundxd <153009580+xddusernotfoundxd@users.noreply.github.com>
2024-10-05 14:41:05 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let but1 = document.querySelector('.but1')
+let but2 = document.querySelector('.but2')
+let but3 = document.querySelector('.but3')
+let but4 = document.querySelector('.but4')
+but1.addEventListener('click', () => {
+	let inp1 = parseFloat(document.querySelector('.inp1').value)
+	let inp2 = parseFloat(document.querySelector('.inp2').value)
+	res = inp1 + inp2
+	document.querySelector('.rez').innerHTML = res
+})
+but2.addEventListener('click', () => {
+	let inp1 = parseFloat(document.querySelector('.inp1').value)
+	let inp2 = parseFloat(document.querySelector('.inp2').value)
+	res = inp1 / inp2
+	document.querySelector('.rez').innerHTML = res
+})
+but3.addEventListener('click', () => {
+	let inp1 = parseFloat(document.querySelector('.inp1').value)
+	let inp2 = parseFloat(document.querySelector('.inp2').value)
+	res = inp1 - inp2
+	document.querySelector('.rez').innerHTML = res
+})
+but4.addEventListener('click', () => {
+	let inp1 = parseFloat(document.querySelector('.inp1').value)
+	let inp2 = parseFloat(document.querySelector('.inp2').value)
+	res = inp1 * inp2
+	document.querySelector('.rez').innerHTML = res
+})
